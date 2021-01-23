9732c95dfd8b671a7feb41c5cd4b1db7a912fdc8
ttprod <77230022+ttprod@users.noreply.github.com>
2021-01-23 12:58:10 +0600

    add

---
+++ TV PLAYER/jquery/script.js
@@ -0,0 +1,6 @@
+$("#button-1").click(function() {
+    $(".modal").toggleClass("hidden").html('<iframe width="100%" height="100%" src="https://ok.ru/videoembed/894544780971?autoplay=1" allowfullscreen frameborder="0"></iframe>');
+ });
+ $("#button-2").click(function() {
+    $(".modal").toggleClass("hidden").html('<iframe width="100%" height="100%" src="https://ok.ru/videoembed/854539312735?autoplay=1" allowfullscreen frameborder="0"></iframe>');
+ });
