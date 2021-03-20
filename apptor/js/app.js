5d5cd750a34722f389f03363330c600d3b953697
ttprod <nadyrbekoff@gmail.com>
2021-03-20 13:48:12 +0600

    update

--- apptor/js/app.js
+++ apptor/js/app.js
@@ -1,6 +1,12 @@
-$(".slide_wrapper").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 1000,
-    nav: true,
-});
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        items: 1,
+        animateOut: 'fadeOut',
+        mouseDrag: false,
+        touchDrad: false,
+    });
+  });
