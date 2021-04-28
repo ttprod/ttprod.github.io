84255656705ddef6bd5fc6671e695671c7890241
ttprod <nadyrbekoff@gmail.com>
2021-04-28 17:19:40 +0600

    edited

--- nameOptions/index.js
+++ nameOptions/index.js
@@ -1,5 +1,3 @@
-import {getNumberOfChars, getFirstChar, getLastChar, getLower, getUpper, getCapitalized, getClean} from "./variations.js";
-
 const answer1 = document.querySelector("#answer1");
 const answer2 = document.querySelector("#answer2");
 const answer3 = document.querySelector("#answer3");
@@ -25,3 +23,38 @@ name.addEventListener("keyup", () => {
     }
     answer7.textContent = getClean(value);
 });
+
+function getNumberOfChars(name) {
+    // return the number of characters in: name
+    return name.length;
+}
+
+function getFirstChar(name) {
+    // return the first character of: name
+    return name[0];
+}
+
+function getLastChar(name) {
+    // return the last character of: name
+    return name[name.length - 1];
+}
+
+function getLower(name) {
+    // return name all in lower case (example: "ABC" becomes "abc")
+    return name.toLowerCase();
+}
+
+function getUpper(name) {
+    // return name all in upper case (example: "abc" becomes "ABC")
+    return name.toUpperCase();
+}
+
+function getCapitalized(name) {
+    // return a capitalized version of name (example: "alEX" becomes "Alex")
+    return name[0].toUpperCase() + name.substring(1).toLowerCase();
+}
+
+function getClean(name) {
+    // return name without trailing and leading space (example: " alex " becomes: "alex")
+    return name.replace(/\s/g, '');
+}
